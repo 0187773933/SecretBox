78008a90e7ae3da4bbbc425d99816bb26d0532f2
0187773933 <collincerbus@student.olympic.edu>
2021-06-14 18:13:02 -0400

    4

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/0187773933/SecretBox
 
 go 1.15
 
-require golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+require (
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+)
--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	secretbox "github.com/0187773933/SecretBox/v1/secretbox"
+	cli "github.com/urfave/cli/v2"
 )
 
-func main() {
-
+func Test() {
 	// box := secretbox.New()
 	// fmt.Println( box.HumanString )
 	// fmt.Println( box.SealMessage( "waduwaduwadu" ) )
@@ -15,6 +16,94 @@ func main() {
 	// fmt.Println( box.OpenMessage( "ZH5ieSI5gYq5MYB+/1RKanrkOxcbZglHEbrMGPsaeEPqrwSQznVvf4YzUhkzRE459lNrzg==" ) )
 	// fmt.Println( box.OpenFile( "/Users/morpheous/WORKSPACE/GO/SecretBox/test.txt" ) )
 
-	box := secretbox.Load( "jDDO/Ew7GdQQPKwFuz3PTZ3I6atG5mjaXlJ8GBV4LOVAQEA9PT1AQEBkfmJ5IjmBirkxgH7/VEpqeuQ7FxtmCUc=" )
-	fmt.Println( box.HumanString )
+	// box := secretbox.Load( "jDDO/Ew7GdQQPKwFuz3PTZ3I6atG5mjaXlJ8GBV4LOVAQEA9PT1AQEBkfmJ5IjmBirkxgH7/VEpqeuQ7FxtmCUc=" )
+	// fmt.Println( box.HumanString )
+}
+
+// https://github.com/urfave/cli/blob/master/docs/v2/manual.md
+func main() {
+
+
+
+	var imported_key string
+	app := &cli.App{
+		Flags: []cli.Flag {
+			&cli.StringFlag{
+				Name: "key",
+				Aliases: []string{ "k" } ,
+				Usage: "Creates New SecretBox" ,
+				Destination: &imported_key ,
+			} ,
+	  	} ,
+		Commands: []*cli.Command {
+			{
+				Name: "new" ,
+				Aliases: []string{ "n" } ,
+				Usage: "Creates New SecretBox" ,
+				Action: func( c *cli.Context ) error {
+					box := secretbox.New()
+					fmt.Println( box.HumanString )
+					return nil
+				} ,
+			} ,
+			{
+				Name: "seal" ,
+				Aliases: []string{ "s" } ,
+				Usage: "seals a plain text message or file" ,
+				Subcommands: []*cli.Command{
+					{
+						Name: "message",
+						Aliases: []string{ "m" } ,
+						Usage: "seals a plain text message" ,
+						Action: func( c *cli.Context ) error {
+							// flag_names := c.FlagNames()
+							// arg_list := c.Args().Slice()
+							box := secretbox.Load( imported_key )
+							fmt.Println( box.SealMessage( c.Args().First() ) )
+							return nil
+						} ,
+					} ,
+					{
+						Name: "file",
+						Aliases: []string{ "f" } ,
+						Usage: "seals a plain text file" ,
+						Action: func( c *cli.Context ) error {
+							arg_list := c.Args().Slice()
+							box := secretbox.Load( imported_key )
+							fmt.Println( box.SealFile( arg_list[ 0 ] , arg_list[ 1 ] ) )
+							return nil
+						} ,
+					} ,
+				} ,
+			} ,
+			{
+				Name: "open" ,
+				Aliases: []string{ "o" } ,
+				Usage: "opens an encrypted text message or file" ,
+				Subcommands: []*cli.Command{
+					{
+						Name: "message",
+						Aliases: []string{ "m" } ,
+						Usage: "opens an encrypted text message" ,
+						Action: func( c *cli.Context ) error {
+							box := secretbox.Load( imported_key )
+							fmt.Println( box.OpenMessage( c.Args().First() ) )
+							return nil
+						} ,
+					} ,
+					{
+						Name: "file",
+						Aliases: []string{ "f" } ,
+						Usage: "opens an encrypted text file" ,
+						Action: func( c *cli.Context ) error {
+							box := secretbox.Load( imported_key )
+							fmt.Println( box.OpenFile( c.Args().First() ) )
+							return nil
+						} ,
+					} ,
+				} ,
+			} ,
+		} ,
+	}
+	app.Run( os.Args )
 }
